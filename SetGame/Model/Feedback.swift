389a96907103fcddb047b32d3ed40ed2dec733ca
David Michaels <dmichaels@gmail.com>
2025-12-19 07:33:57 -0500

    feedback.swift

---
+++ SetGame/Model/Feedback.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+import AudioToolbox
+import CoreHaptics
+import AVFoundation
+
+public class Feedback: ObservableObject
+{
+    public var soundsEnabled: Bool = false
+    public var hapticsEnabled: Bool = false
+
+    public init(sounds: Bool = false, haptics: Bool = false) {
+        self.soundsEnabled = sounds
+        self.hapticsEnabled = haptics
+    }
+    
+    public func triggerTapSound() {
+        do {
+            let session = AVAudioSession.sharedInstance()
+            try session.setCategory(.playback, mode: .default, options: .mixWithOthers)
+            try session.setActive(true)
+            AudioServicesPlaySystemSound(1104)
+        } catch {
+            print("Error setting audio session: \(error)")
+        }
+    }
+    
+    public func triggerHaptic() {
+        let generator = UIImpactFeedbackGenerator(style: .light)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+
+    public func trigger() {
+        if (self.soundsEnabled) {
+            self.triggerTapSound()
+        }
+        if (self.hapticsEnabled) {
+            self.triggerHaptic()
+        }
+    }
+}
