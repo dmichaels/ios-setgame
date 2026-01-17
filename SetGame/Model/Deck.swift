69d98e012b056346d3836a6f943f983c28200e42
David Michaels <dmichaels@gmail.com>
2026-01-17 08:00:08 -0500

    minor-refactor

--- SetGame/Model/Deck.swift
+++ SetGame/Model/Deck.swift
@@ -90,19 +90,4 @@ public class Deck<T : Card> {
     func takeRandomCards(_ n : Int, plantSet: Bool = false, existingCards: [T] = []) -> [T] {
         return self.readonly ? [T]() : self.cards.takeRandomCards(n, plantSet: plantSet, existingCards: existingCards);
     }
-
-    /// Returns (WITHOUT removal) a random magic SET square from a full deck.
-    /// N.B. Currently ONLY for the purpose of constructing a magic square.
-    ///
-    static func randomMagicSquare(simple: Bool = false) -> [Card] {
-        let deck: [Card] = simple ? TableDeck.instanceSimple.cards : TableDeck.instance.cards;
-        var magic: [Card] = deck.randomNonSetCards();
-        magic.append(Card.matchingSetValue(magic[0], magic[1])); // [3] from [0] and [1]
-        magic.append(Card.matchingSetValue(magic[0], magic[2])); // [4] from [0] and [2]
-        magic.append(Card.matchingSetValue(magic[3], magic[4])); // [5] from [3] and [4]
-        magic.append(Card.matchingSetValue(magic[2], magic[5])); // [6] from [2] and [5]
-        magic.append(Card.matchingSetValue(magic[1], magic[5])); // [7] from [1] and [5)
-        magic.append(Card.matchingSetValue(magic[0], magic[5])); // [8] from [0] and [5]
-        return [magic[0], magic[1], magic[3], magic[2], magic[5], magic[6], magic[4], magic[7], magic[8]]
-    }
 }
--- SetGame/Model/TableDeck.swift
+++ SetGame/Model/TableDeck.swift
@@ -1,4 +1,20 @@
 public class TableDeck: Deck<TableCard> {
-    static public let instance       : TableDeck = TableDeck();
-    static public let instanceSimple : TableDeck = TableDeck(simple: true);
+
+    public static let instance       : TableDeck = TableDeck();
+    public static let instanceSimple : TableDeck = TableDeck(simple: true);
+
+    /// Returns (WITHOUT removal) a random magic SET square from a full deck.
+    /// N.B. Currently ONLY for the purpose of constructing a magic square.
+    ///
+    public static func randomMagicSquare(simple: Bool = false) -> [Card] {
+        let deck: [Card] = simple ? TableDeck.instanceSimple.cards : TableDeck.instance.cards;
+        var magic: [Card] = deck.randomNonSetCards();
+        magic.append(Card.matchingSetValue(magic[0], magic[1])); // [3] from [0] and [1]
+        magic.append(Card.matchingSetValue(magic[0], magic[2])); // [4] from [0] and [2]
+        magic.append(Card.matchingSetValue(magic[3], magic[4])); // [5] from [3] and [4]
+        magic.append(Card.matchingSetValue(magic[2], magic[5])); // [6] from [2] and [5]
+        magic.append(Card.matchingSetValue(magic[1], magic[5])); // [7] from [1] and [5)
+        magic.append(Card.matchingSetValue(magic[0], magic[5])); // [8] from [0] and [5]
+        return [magic[0], magic[1], magic[3], magic[2], magic[5], magic[6], magic[4], magic[7], magic[8]]
+    }
 }
