4a9077f473e7230f8e33f1412dcd54184e758d40
David Michaels <dmichaels@gmail.com>
2026-01-19 10:38:44 -0500

    card-grid-view

---
+++ SetGame/Views/CardGridView.swift
@@ -0,0 +1,63 @@
+import SwiftUI
+
+public struct CardGridView: View {
+
+    @ObservedObject var table: Table;
+    @ObservedObject var settings: Settings;
+
+    var spacing: CGFloat = 8;
+    var marginx: CGFloat = 8;
+
+    public var body: some View {
+        let spacingx: CGFloat = spacing;
+        let spacingy: CGFloat = spacing;
+        //
+        // Spacing notes:
+        // - marginx
+        //   The HStack spacing is amount of horizontal space to
+        //   the left and right of the table card grid itself;
+        //   also requires Spacer before/after the LazyVGrid.
+        // - spacingx
+        //   The columns/GridItem array spacing is the horizontal
+        //   space between each card on the table card grid.
+        // - spacingy
+        //   The LazyVGrid spacing is the vertical space
+        //   between each card (row) on the table card grid.
+        //
+        HStack(spacing: marginx) {
+            let columns: Array<GridItem> = Array(
+                repeating: GridItem(.flexible(), spacing: spacing),
+                count: settings.cardsPerRow
+            )
+            Spacer()
+            LazyVGrid(columns: columns, spacing: spacing) {
+                ForEach(table.cards, id: \.id) { card in
+                    CardView(
+                        card,
+                        materialize: true,
+                        askew: settings.cardsAskew,
+                        alternate: settings.alternateCards
+                    ) { card in
+                        self.table.cardTouched(
+                            card,
+                            delay: Defaults.Effects.selectDelay,
+                            onSet: { cards, resolve in
+                                cards.blink() {
+                                    resolve();
+                                }
+                            },
+                            onNoSet: { cards, resolve in
+                                cards.shake();
+                                resolve();
+                            },
+                            onCardsMoved: { cards in
+                                cards.flip(duration: 0.8);
+                            }
+                        )
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+}
--- SetGame/Views/TableView.swift
+++ SetGame/Views/TableView.swift
@@ -16,7 +16,7 @@ public struct TableView: View {
 
     public var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            CardGrid(table: table, settings: settings, spacing: spacing, marginx: marginx)
+            CardGridView(table: table, settings: settings, spacing: spacing, marginx: marginx)
             Space(size: 18)
             StatusBar(marginx: marginx)
             Space(size: 12)
@@ -24,68 +24,6 @@ public struct TableView: View {
             MultiPlayerGameButton()
         }
         .allowsHitTesting(!self.table.state.disabled)
-    }
-
-    private struct CardGrid: View  {
-
-        @ObservedObject var table: Table;
-        @ObservedObject var settings: Settings;
-
-        var spacing: CGFloat = 8;
-        var marginx: CGFloat = 8;
-
-        public var body: some View {
-            let spacingx: CGFloat = spacing;
-            let spacingy: CGFloat = spacing;
-            //
-            // Spacing notes:
-            // - marginx
-            //   The HStack spacing is amount of horizontal space to
-            //   the left and right of the table card grid itself;
-            //   also requires Spacer before/after the LazyVGrid.
-            // - spacingx
-            //   The columns/GridItem array spacing is the horizontal
-            //   space between each card on the table card grid.
-            // - spacingy
-            //   The LazyVGrid spacing is the vertical space
-            //   between each card (row) on the table card grid.
-            //
-            HStack(spacing: marginx) {
-                let columns: Array<GridItem> = Array(
-                    repeating: GridItem(.flexible(), spacing: spacing),
-                    count: settings.cardsPerRow
-                )
-                Spacer()
-                LazyVGrid(columns: columns, spacing: spacing) {
-                    ForEach(table.cards, id: \.id) { card in
-                        CardView(
-                            card,
-                            materialize: true,
-                            askew: settings.cardsAskew,
-                            alternate: settings.alternateCards
-                        ) { card in
-                            self.table.cardTouched(
-                                card,
-                                delay: Defaults.Effects.selectDelay,
-                                onSet: { cards, resolve in
-                                    cards.blink() {
-                                        resolve();
-                                    }
-                                },
-                                onNoSet: { cards, resolve in
-                                    cards.shake();
-                                    resolve();
-                                },
-                                onCardsMoved: { cards in
-                                    cards.flip(duration: 0.8);
-                                }
-                            )
-                        }
-                    }
-                }
-                Spacer()
-            }
-        }
     }
 
     private struct StatusBar: View {
