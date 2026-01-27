03fb54367d7fec9933b9fbbf3bbbab9b46f774ac
David Michaels <dmichaels@gmail.com>
2026-01-27 15:14:04 -0500

    game-center-http-based

--- SetGame/SetGameApp.swift
+++ SetGame/SetGameApp.swift
@@ -6,16 +6,14 @@ struct SetGameApp: App {
     @StateObject private var settings: Settings = Settings();
     @StateObject private var feedback: Feedback;
     @StateObject private var table: Table;
-        // public static var gameCenterTransport: GameCenter.Transport? = nil;
 
     init() {
-        // SetGameApp.gameCenterTransport = Defaults.gameCenter ? GameCenter.HttpTransport(player: "A") : nil;
         let settings: Settings = Settings();
         _settings = StateObject(wrappedValue: settings);
         _feedback = StateObject(wrappedValue: Feedback(sounds: settings.sounds,
                                                        haptics: settings.haptics));
-        // _table = StateObject(wrappedValue: Table(settings: settings, gameCenterSender: SetGameApp.gameCenterTransport));
-        _table = StateObject(wrappedValue: Table(settings: settings, gameCenterSender: GameCenter.HttpTransport.instance));
+        _table = StateObject(wrappedValue: Table(settings: settings,
+                                                 gameCenterSender: GameCenter.HttpTransport.instance));
     }
 
     var body: some Scene {
@@ -26,7 +24,6 @@ struct SetGameApp: App {
                 .environmentObject(self.feedback)
                 .task {
                     await GameCenterAuthentication.authenticate();
-                    // SetGameApp.gameCenterTransport?.configure(handler: self.table);
                     GameCenter.HttpTransport.instance.configure(handler: self.table);
                 }
         }
