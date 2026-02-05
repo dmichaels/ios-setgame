881b8cea08e512c534bca773091ded208b16571d
David Michaels <dmichaels@gmail.com>
2026-02-05 11:36:34 -0500

    etc

--- SetGame/Utils/RNG.swift
+++ SetGame/Utils/RNG.swift
@@ -103,10 +103,7 @@ public class RNG {
 
     public func next(in range: Range<Int>) -> Int {
         if let rng = self.rng {
-            // return range.lowerBound + rng.nextInt(upperBound: range.upperBound - range.lowerBound);
-            let x = range.lowerBound + rng.nextInt(upperBound: range.upperBound - range.lowerBound);
-            deb("RNG-NEXT-A: \(range) -> \(x)")
-            return x
+            return range.lowerBound + rng.nextInt(upperBound: range.upperBound - range.lowerBound);
         }
         else {
             return Int.random(in: range);
@@ -118,10 +115,7 @@ public class RNG {
             let lower: Int = range.lowerBound;
             let upper: Int = range.upperBound;
             let span: Int = max(1, (upper == Int.max ? upper - 1 : upper) - lower + 1);
-            // return range.lowerBound + rng.nextInt(upperBound: span);
-            let x = range.lowerBound + rng.nextInt(upperBound: span);
-            deb("RNG-NEXT-B: \(range) -> \(x)")
-            return x
+            return range.lowerBound + rng.nextInt(upperBound: span);
         }
         else {
             return Int.random(in: range);
