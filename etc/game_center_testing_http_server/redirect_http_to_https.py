ea699096c458f04a80b08cb32cd4e74dfcecfe61
David Michaels <dmichaels@gmail.com>
2026-02-06 08:34:52 -0500

    etc

---
+++ etc/game_center_testing_http_server/redirect_http_to_https.py
@@ -0,0 +1,12 @@
+# redirect_http_to_https.py
+from flask import Flask, redirect, request
+
+app = Flask(__name__)
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def redirect_to_https(path):
+    return redirect(f"https://{request.host}{request.full_path}", code=301)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
---
+++ etc/game_center_testing_http_server/redirect_http_to_https.sh
@@ -0,0 +1,1 @@
+sudo -E python3 redirect_http_to_https.py > redirect_http_to_https.log 2>&1 &
