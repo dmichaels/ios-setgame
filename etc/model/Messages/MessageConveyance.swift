562573313f7a888ac14c7752b47923b5ed1157df
David Michaels <dmichaels@gmail.com>
2026-02-11 02:48:53 -0500

    etc

---
+++ etc/model/Messages/MessageConveyance.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public struct MessageConveyance {
+
+    public static func dispatch(messages: [Message]?, handler: MessageHandler) {
+        if let messages: [Message] = messages {
+            for message: Message in messages {
+                MessageConveyance.dispatch(message: message,
+                                           ping: handler.handle,
+                                           joinSession: handler.handle,
+                                           joinedSession: handler.handle);
+            }
+        }
+    }
+
+    private static func dispatch(message: Message?,
+                                ping: ((PingMessage) -> Void)? = nil,
+                                joinSession: ((JoinSessionMessage) -> Void)? = nil,
+                                joinedSession: ((JoinedSessionMessage) -> Void)? = nil) {
+        if let message: Message = message {
+            switch message {
+                case let message as PingMessage: ping?(message);
+                case let message as JoinSessionMessage: joinSession?(message);
+                case let message as JoinedSessionMessage: joinedSession?(message);
+                default: break;
+            }
+        }
+    }
+}
