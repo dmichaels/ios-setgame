550e75f839845a85360a1c7b8e30c86e4a65e1ac
David Michaels <dmichaels@gmail.com>
2026-02-13 17:21:21 -0500

    etc

---
+++ SetGame/MultiPlayer/Dev/DevPanel.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+public extension MultiPlayer {
+
+    @ObservedObject var table: Table
+    @ObservedObject var settings: Settings;
+
+    public struct DevPanel: View {
+        public var body: some View {
+        }
+    }
+}
