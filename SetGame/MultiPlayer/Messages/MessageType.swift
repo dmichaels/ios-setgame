f76b3f580127ef460fb0ce60ca51ed118ae2f7dd
David Michaels <dmichaels@gmail.com>
2026-02-13 14:53:09 -0500

    new-game-center-modules-multi-player-namespace

--- SetGame/MultiPlayer/Messages/MessageType.swift
+++ SetGame/MultiPlayer/Messages/MessageType.swift
@@ -12,15 +12,15 @@ public extension MultiPlayer {
 
 public extension MultiPlayer.SessionHandler {
     //
-    // These player joining related message handlers are defaulted so
-    // that the main MessageHandler, i.e. Table in our case, does not have
+    // These session management related message handlers are defaulted so
+    // that the main SessionHandler, i.e. Table in our case, does not have
     // to bother implementing these, since this should be of no concern there;
-    // these are instead handled directly by the Session implementation.
+    // these are instead handled directly by the HttpSession implementation.
     //
-    func handle(message: MultiPlayer.PingMessage) {}
     func handle(message: MultiPlayer.JoinSessionMessage) {}
     func handle(message: MultiPlayer.JoinSessionConfirmedMessage) {}
     func handle(message: MultiPlayer.LeaveSessionMessage) {}
     func handle(message: MultiPlayer.RequestHostSessionMessage) {}
     func handle(message: MultiPlayer.UpdateSessionMessage) {}
+    func handle(message: MultiPlayer.PingMessage) {}
 }
