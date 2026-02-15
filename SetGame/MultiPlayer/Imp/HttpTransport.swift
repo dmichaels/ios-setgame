3adec5386a646d6293b3b985e48e2088235f81d7
David Michaels <dmichaels@gmail.com>
2026-02-15 13:56:53 -0500

    etc

--- SetGame/MultiPlayer/Imp/HttpTransport.swift
+++ SetGame/MultiPlayer/Imp/HttpTransport.swift
@@ -192,9 +192,7 @@ public extension MultiPlayer {
         }
 
         public static func messageCounts(info: Json, player: String) -> (sent: Int, queued: Int, received: Int) {
-            var sent: Int = 0;
-            var queued: Int = 0;
-            var received: Int = 0;
+            var sent: Int = 0; var queued: Int = 0; var received: Int = 0;
             if let count: Json = info["sent_count"] as? Json, let count = count[player] as? Int {
                 sent = count;
             }
