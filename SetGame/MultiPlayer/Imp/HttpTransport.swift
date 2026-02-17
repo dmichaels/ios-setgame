57a11dc22a321ee2a81007bfb5abf16b095d45e1
David Michaels <dmichaels@gmail.com>
2026-02-16 22:15:04 -0500

    etc

--- SetGame/MultiPlayer/Imp/HttpTransport.swift
+++ SetGame/MultiPlayer/Imp/HttpTransport.swift
@@ -223,8 +223,8 @@ public extension MultiPlayer {
                            let messages: [Json] = messagesItem["messages"] as? [Json] {
                             for message in messages {
                                 if let message = MessageConversion.toMessage(json: message) {
-                                    result.append(MessageReceived(
-                                        timestamp: substring(timestamp, from: 11, length: 11), host: host, message: message));
+                                    result.append(MessageReceived(timestamp: timestamp.substring(from: 11, length: 11),
+                                                                  host: host, message: message));
                                     print(message)
                                 }
                             }
--- SetGame/Utils/String.swift
+++ SetGame/Utils/String.swift
@@ -13,4 +13,11 @@ extension String {
             .map { $0.trimmingCharacters(in: .whitespaces) }
             .filter { !$0.isEmpty }
     }
+
+    public func substring(from start: Int, length: Int) -> String {
+        guard start >= 0, length >= 0, start < self.count else { return "" }
+        let start = self.index(self.startIndex, offsetBy: start)
+        let end = self.index(start, offsetBy: length, limitedBy: self.endIndex) ?? self.endIndex;
+        return String(self[start..<end])
+    }
 }
