e4c121899d5c5dfb8a74e3b83b2bdc36e6593a39
David Michaels <dmichaels@gmail.com>
2026-02-18 22:38:41 -0500

    etc

--- SetGame/MultiPlayer/Messages/PingAcknowledgeMessage.swift
+++ SetGame/MultiPlayer/Messages/PingAcknowledgeMessage.swift
@@ -4,9 +4,11 @@ public extension MultiPlayer {
 
     public class PingAcknowledgeMessage: Message {
         public let type: MessageType;
+        public let from: String;
         public let id: String;
-        public init(id: String) {
+        public init(from: String, id: String) {
             self.type = .pingAcknowledge;
+            self.from = from;
             self.id = id;
         }
     }
--- SetGame/MultiPlayer/Messages/PingMessage.swift
+++ SetGame/MultiPlayer/Messages/PingMessage.swift
@@ -4,9 +4,12 @@ public extension MultiPlayer {
 
     public class PingMessage: Message {
         public let type: MessageType;
-        public let id: String = ID().value;
-        public init() {
+        public let from: String;
+        public let id: String;
+        public init(from: String, id: String = ID().value) {
             self.type = .ping;
+            self.from = from;
+            self.id = id;
         }
     }
 }
